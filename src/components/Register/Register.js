5e00efd6ccbd80575de5eb3d63269085f29f0681
Jessica Myer <36251741+oeddiemeddie@users.noreply.github.com>
2020-05-20 20:39:31 -0600

    frontend register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,73 +1,76 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 import { register } from "../../ducks/userReducer"
 import { connect } from 'react-redux'
 
 
-function Register(props) {
-    const [email, setEmail] = useState('')
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [password, setPassword] = useState('')
-
-    const handleEmail = (e) => {
-        setEmail(e.target.value)
+class Register extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: "",
+            first_name: "",
+            last_name: "",
+            password: "",
+        }
     }
 
-    const handleFirstName = (e) => {
-        setFirstName(e.target.value)
+    handleInput = (e) => {
+        this.setState({
+            [e.target.name]:e.target.value
+        })
     }
 
-    const handleLastName = (e) => {
-        setLastName(e.target.value)
+    handleRegister = (e) => {
+        const { email, first_name, last_name, password } = this.state
+        axios.post('/api/register', {
+            email,
+            first_name,
+            last_name,
+            password,
+        })
+        .then(res => {
+            this.props.history.push('/pantry')
+        })
+        .catch(err => console.log(err))
     }
 
-    const handlePassword = (e) => {
-        setPassword(e.target.value)
+    render() {
+        return (
+            <div className="register-comp">
+                <h1>Register</h1>
+                <form id='register-form'>
+                    <label htmlFor='firstName'>First Name</label>
+                    <input
+                        type='text'
+                        name='firstName'
+                        placeholder='First Name'
+                        onClick={handleInput} />
+                    <label htmlFor='lastName'>Last Name</label>
+                    <input
+                        type='text'
+                        name='lastName'
+                        placeholder='Last Name'
+                        onClick={handleInput} />
+                    <label htmlFor='email'>Email</label>
+                    <input
+                        type='email'
+                        name='email'
+                        placeholder='Email'
+                        onClick={handleInput} />
+                    <label htmlFor='password'>Password</label>
+                    <input 
+                        type='password'
+                        name='password'
+                        placeholder='Password'
+                        onClick={handleInput} />
+                    <button onClick={handleRegister}>Submit</button>
+                    <a href='/'>I already have an account</a>
+                </form>
+            </div>
+        )
     }
 
-    const registerUser = (e) => {
-        e.preventDefault()
-        props.register(email, firstName, lastName, password)
-        setEmail('')
-        setFirstName('')
-        setLastName('')
-        setPassword('')
-        props.history.push('/pantry')
-    }
-
-    return (
-        <div className="register-comp">
-            <h1>Register</h1>
-            <form id='register-form'>
-                <label htmlFor='firstName'>First Name</label>
-                <input
-                    type='text'
-                    name='firstName'
-                    placeholder='First Name'
-                    onClick={handleFirstName} />
-                <label htmlFor='lastName'>Last Name</label>
-                <input
-                    type='text'
-                    name='lastName'
-                    placeholder='Last Name'
-                    onClick={handleLastName} />
-                <label htmlFor='email'>Email</label>
-                <input
-                    type='email'
-                    name='email'
-                    placeholder='Email'
-                    onClick={handleEmail} />
-                <label htmlFor='password'>Password</label>
-                <input 
-                    type='password'
-                    name='password'
-                    placeholder='Password'
-                    onClick={handlePassword} />
-                <button onClick={registerUser}>Submit</button>
-                <a href='/'>I already have an account</a>
-            </form>
-        </div>
-    )
 }
 
 const mapStateToProps = reduxState => {
